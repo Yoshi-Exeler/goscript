f84f21d4216112889e46d7c7d7691af834520759
Yoshi-Exeler <34031242+Yoshi-Exeler@users.noreply.github.com>
2022-06-07 18:18:08 +0200

    [FIX] move test to depresolve_test.go

---
+++ pkg/goscript/depresolve_test.go
@@ -0,0 +1,16 @@
+package goscript
+
+import (
+	"testing"
+)
+
+// TestGetRequiredExternals parses the externals of the specified file
+func TestGetRequiredExternals(t *testing.T) {
+	ret, err := getRequiredExternals("../../tests/externals.gs")
+	if err != nil {
+		t.Fatalf("got error %v", err)
+	}
+	if len(ret) != 1 {
+		t.Fatalf("expected one externals but got %v", len(ret))
+	}
+}
--- pkg/goscript/discovery_test.go
+++ pkg/goscript/discovery_test.go
@@ -4,17 +4,6 @@ import (
 	"testing"
 )
 
-// TestGetRequiredExternals parses the externals of the specified file
-func TestGetRequiredExternals(t *testing.T) {
-	ret, err := getRequiredExternals("../../tests/externals.gs")
-	if err != nil {
-		t.Fatalf("got error %v", err)
-	}
-	if len(ret) != 1 {
-		t.Fatalf("expected one externals but got %v", len(ret))
-	}
-}
-
 func TestSourceWalk(t *testing.T) {
 	_, err := DiscoverSources("../../tests/externals.gs", "../../tests/")
 	if err != nil {

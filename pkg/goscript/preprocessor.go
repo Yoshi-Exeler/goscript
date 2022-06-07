25f42515fc66859436955b09b6d47325adddca85
Yoshi-Exeler <yoshi@exeler.org>
2022-06-07 10:55:29 +0200

    [ADD] add preprocessor spec

--- pkg/goscript/preprocessor.go
+++ pkg/goscript/preprocessor.go
@@ -2,4 +2,14 @@ package goscript
 
 // this package will contain the preprocessor for goscript
 
-func PreprocessSources(sourceFiles []string) string { return "" }
+/* PreprocessAppSource will apply preprocessing steps to the applications source code
+   The following steps will be applied:
+	- delete all comments
+	- delete import directives
+	- delete application directive
+	- delete external directives
+	- prefix all symbols with the hash of their module (a function getJWT from the package jwt would become hash_getJWT)
+	- replace external symbol references with their expected name (jwt.getJWT becomes hash_getJWT)
+	- merge all source files
+*/
+func PreprocessAppSource(source *ApplicationSource) string { return "" }

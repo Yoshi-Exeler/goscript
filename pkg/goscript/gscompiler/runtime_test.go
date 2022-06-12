2f78a19537ff0192e8da1313e2f700a211552743
Yoshi-Exeler <34031242+Yoshi-Exeler@users.noreply.github.com>
2022-06-12 17:24:54 +0200

    [ADD] add testAssignFunctionReturnValue

--- pkg/goscript/gscompiler/runtime_test.go
+++ pkg/goscript/gscompiler/runtime_test.go
@@ -5,10 +5,42 @@ import (
 	"testing"
 )
 
-func TestAssignSymbol(t *testing.T) {
+/*
+	func main() {
+		let a: uint8 = 11
+	}
+*/
+func TestAssignConstant(t *testing.T) {
 	testProgram := Program{
-		Operations:      []BinaryOperation{*NewAssignExpressionOp(1, NewConstantExpression(uint8(11), BT_INT8))},
+		Operations:      []BinaryOperation{*NewAssignExpressionOp(1, NewConstantExpression(uint8(11), BT_UINT8))},
 		SymbolTableSize: 2,
+	}
+	runtime := NewRuntime()
+	runtime.Exec(testProgram)
+	if runtime.SymbolTable[1].Value.(uint8) != 11 {
+		t.Fatalf("symbol should have been 11 but was %v", runtime.SymbolTable[1].Value.(uint8))
+	}
+	fmt.Printf("%+v\n", *runtime.SymbolTable[1])
+}
+
+/*
+	func main() {
+		let a: uint8 = getConst()
+	}
+
+	func getConst() => uint8 {
+		let b: uint8 = 11
+		return b
+	}
+*/
+func TestAssignFunctionReturnValue(t *testing.T) {
+	testProgram := Program{
+		Operations: []BinaryOperation{
+			*NewAssignExpressionOp(1, NewFunctionExpression(1, BT_UINT8, []*FunctionArgument{})), // assign the return value of the function at pc1 to the symbol 1
+			*NewAssignExpressionOp(2, NewConstantExpression(uint8(11), BT_UINT8)),                // assign the constant 11 to the local symbol 2
+			*NewReturnValueOp(NewVSymbolExpression(2, BT_UINT8)),                                 // return the value of the symbol 2
+		},
+		SymbolTableSize: 10,
 	}
 	runtime := NewRuntime()
 	runtime.Exec(testProgram)

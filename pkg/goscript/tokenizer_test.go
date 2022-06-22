e0365928fef23358060a66e947daff4a9d1d8241
Yoshi Exeler <yoshi.exeler@intellitrend.de>
2022-06-22 09:03:10 +0200

    [ADD] add tokenizer_test.go

---
+++ pkg/goscript/tokenizer_test.go
@@ -0,0 +1,36 @@
+package goscript
+
+import (
+	"testing"
+)
+
+var stub1 = `
+func main() {
+    let x: string = "db.Connect"
+    let y: string = "db.Connect"
+    fn_ad923fa3_db_Connect()
+}
+func fn_ad923fa3_db_doSomeDBStuff() => uint64 {
+    return 0
+}
+func fn_ad923fa3_db_Connect() {
+    return
+}
+
+func fn_c40313b4_math_add(a: uint64, b: uint64) => uint64 {
+    return a + b
+}
+
+func fn_e0e93cde_crypto_MakeJWT() {
+    return
+}
+
+func fn_a9d72907_jwt_getSomeJWT() => uint64 {
+    return 1
+}
+`
+
+func TestFindFunctions(t *testing.T) {
+	tokenizer := &Tokenizer{}
+	tokenizer.parse(stub1)
+}

81f65824d71507583beb315b15e88b450141fd4a
Yoshi Exeler <yoshi.exeler@intellitrend.de>
2022-09-06 12:47:09 +0200

    [ADD] add missing gob register

--- pkg/goscript/bytecode.go
+++ pkg/goscript/bytecode.go
@@ -19,7 +19,7 @@ func (p *Program) Encode(out string) {
 	}
 	gob.Register(BT_ANY)
 	gob.Register(Expression{})
-	gob.Register(BinaryTypedValue{})
+	gob.Register([]*BinaryTypedValue{})
 	enc := gob.NewEncoder(f)
 	err = enc.Encode(p)
 	if err != nil {
